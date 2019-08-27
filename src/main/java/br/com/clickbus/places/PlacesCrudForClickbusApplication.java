3229bd056f1511877e79b3a5aa9afae72cb1e6d7
llanziloti <llanziloti@unicid.edu.br>
2019-08-27 08:33:14 -0300

    Creating Archetype v0.0.1

---
+++ src/main/java/br/com/clickbus/places/PlacesCrudForClickbusApplication.java
@@ -0,0 +1,13 @@
+package br.com.clickbus.places;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesCrudForClickbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacesCrudForClickbusApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/clickbus/places/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.com.clickbus.places;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PlacesCrudForClickbusApplication.class);
+	}
+
+}
