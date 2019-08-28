7142a9f19c2a48acf9bc4c3e0101d228680cf551
llanziloti <llanziloti@unicid.edu.br>
2019-08-28 13:19:04 -0300

    Removing /target

--- target/classes/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO Place(name, slug, city, state, creation_date) VALUES('DISNEY', 'SLUG', 'ORLANDO', 'FLORIDA', SYSDATE());
-INSERT INTO Place(name, slug, city, state, creation_date) VALUES('MORUMBI', 'SLUG', 'SÃO PAULO', 'SÃO PAULO', SYSDATE());
-INSERT INTO Place(name, slug, city, state, creation_date) VALUES('GOLDEN STATE', 'SLUG', 'SAN FRANCISO', 'CALIFORNIA', SYSDATE());
-
-
